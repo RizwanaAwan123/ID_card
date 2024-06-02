7956ef4fe397701fc8cfa229ac781a14b5c4eff0
RizwanaAwan123 <awanareeb34@gmail.com>
2024-06-02 12:59:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//object 
+let myname = "Rizwana Awan";
+let Roll_No = 286898;
+let myage = 26;
+let mygender = "female";
+let Distance = "no";
+let center = "Governer House Karachi";
+let campus = "Main Campus";
+let Datetime = "10-11-2023";
+let Batch = 1;
+let mydata = {
+    name: myname, Roll_No: Roll_No,
+    age: myage,
+    gender: mygender, Distance: Distance,
+    center: center,
+    Campus: campus, Datetime: Datetime,
+    Batch: Batch
+};
+console.log(mydata.Batch);
+console.log([mydata.Batch, mydata.Datetime, mydata.center, mydata.Campus, mydata.Distance, mydata.gender, mydata.age, mydata.Roll_No, mydata.name]);
